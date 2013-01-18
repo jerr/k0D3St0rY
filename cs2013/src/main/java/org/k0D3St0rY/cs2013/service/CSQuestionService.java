7c9cc052f8d6479a68728da3428314207209d480
Jeremie Lagarde <jer@printstacktrace.org>
2013-01-18 21:45:53 +0100

    #4 : fifth question

--- cs2013/src/main/java/org/k0D3St0rY/cs2013/service/CSQuestionService.java
+++ cs2013/src/main/java/org/k0D3St0rY/cs2013/service/CSQuestionService.java
@@ -6,25 +6,26 @@ import java.util.Properties;
 
 public class CSQuestionService extends AbstractCSService {
 
-    // I like simple code! 
+    // I like simple code!
     private final static Properties questions = new Properties();
     private final static String error = "Sorry, I don't understand ...";
-    
+
     public CSQuestionService() {
-        questions.put("Quelle est ton adresse email","jeremie.codestory@gmail.com");
-        questions.put("Es tu heureux de participer(OUI/NON)","OUI");
+        questions.put("Quelle est ton adresse email", "jeremie.codestory@gmail.com");
+        questions.put("Es tu heureux de participer(OUI/NON)", "OUI");
         questions.put("Es tu abonne a la mailing list(OUI/NON)", "OUI");
-        questions.put("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)","OUI");
+        questions.put("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)", "OUI");
+        questions.put("Est ce que tu reponds toujours oui(OUI/NON)", "NON");
     }
-    
+
     @Override
     public CharSequence execute(Map<String, List<String>> params) {
-        if (!params.isEmpty() && params.containsKey("q") ){
+        if (!params.isEmpty() && params.containsKey("q")) {
             return questions.getProperty(params.get("q").get(0), error);
         } else {
             return error;
         }
-        
+
     }
 
 }
