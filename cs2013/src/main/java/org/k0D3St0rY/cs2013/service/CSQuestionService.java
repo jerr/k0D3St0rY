ec7321f6335f6666e89bcea0aa110cb587cab35b
Jeremie Lagarde <jer@printstacktrace.org>
2013-01-22 22:28:42 +0100

    #8 : Operator

--- cs2013/src/main/java/org/k0D3St0rY/cs2013/service/CSQuestionService.java
+++ cs2013/src/main/java/org/k0D3St0rY/cs2013/service/CSQuestionService.java
@@ -1,13 +1,16 @@
 package org.k0D3St0rY.cs2013.service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.jboss.netty.util.internal.StringUtil;
+
 public class CSQuestionService extends AbstractCSService {
 
-    // I like simple code!
-    private final static Properties questions = new Properties();
+    private final static Map<String,String> questions = new HashMap<String, String>();
     private final static String error = "Sorry, I don't understand ...";
 
     public CSQuestionService() {
@@ -17,17 +20,33 @@ public class CSQuestionService extends AbstractCSService {
         questions.put("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)", "OUI");
         questions.put("Est ce que tu reponds toujours oui(OUI/NON)", "NON");
         questions.put("As tu bien recu le premier enonce(OUI/NON)", "OUI");
-        questions.put("1 1", "2");
     }
 
     @Override
     public CharSequence execute(Map<String, List<String>> params) {
         if (!params.isEmpty() && params.containsKey("q")) {
-            return questions.getProperty(params.get("q").get(0), error);
+            String key = params.get("q").get(0);
+            if(questions.containsKey(key))
+                return questions.get(key);
+            else {
+                key = key.replace(',', '.');
+                if(key.contains(" ")) {
+                    String[] values = StringUtil.split(key, ' ');
+                    return (new BigDecimal(values[0])).add(new BigDecimal(values[1])).toString();
+                } if(key.contains("-")) {
+                    String[] values = StringUtil.split(key, '-');
+                    return (new BigDecimal(values[0])).min(new BigDecimal(values[1])).toString();
+                } if(key.contains("*")) {
+                    String[] values = StringUtil.split(key, '*');
+                    return (new BigDecimal(values[0])).multiply(new BigDecimal(values[1])).toString();
+                } if(key.contains("/")) {
+                    String[] values = StringUtil.split(key, '/');
+                    return (new BigDecimal(values[0])).divide(new BigDecimal(values[1])).toString();
+                } return error;
+            }
         } else {
             return error;
         }
 
     }
-
 }
