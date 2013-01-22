13cee13c36cd3270854adca8e609de493a2ae259
Jeremie Lagarde <jer@printstacktrace.org>
2013-01-22 01:52:24 +0100

    dump data

--- cs2013/src/main/java/org/k0D3St0rY/cs2013/server/HttpCSHandler.java
+++ cs2013/src/main/java/org/k0D3St0rY/cs2013/server/HttpCSHandler.java
@@ -1,47 +1,43 @@
 package org.k0D3St0rY.cs2013.server;
 
-import java.io.IOException;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.getHost;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.is100ContinueExpected;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.isKeepAlive;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.OK;
+import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
 import java.net.URI;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Map.Entry;
 
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
-import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelFutureListener;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 import org.jboss.netty.channel.group.ChannelGroup;
-import org.jboss.netty.handler.codec.http.Cookie;
-import org.jboss.netty.handler.codec.http.CookieDecoder;
-import org.jboss.netty.handler.codec.http.CookieEncoder;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
 import org.jboss.netty.handler.codec.http.HttpChunk;
 import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponse;
 import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.jboss.netty.handler.codec.http.QueryStringDecoder;
-import org.jboss.netty.handler.codec.http.multipart.Attribute;
 import org.jboss.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
 import org.jboss.netty.handler.codec.http.multipart.DiskFileUpload;
-import org.jboss.netty.handler.codec.http.multipart.FileUpload;
 import org.jboss.netty.handler.codec.http.multipart.HttpDataFactory;
 import org.jboss.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
-import org.jboss.netty.handler.codec.http.multipart.HttpPostRequestDecoder.EndOfDataDecoderException;
-import org.jboss.netty.handler.codec.http.multipart.HttpPostRequestDecoder.ErrorDataDecoderException;
-import org.jboss.netty.handler.codec.http.multipart.HttpPostRequestDecoder.IncompatibleDataDecoderException;
-import org.jboss.netty.handler.codec.http.multipart.HttpPostRequestDecoder.NotEnoughDataDecoderException;
-import org.jboss.netty.handler.codec.http.multipart.InterfaceHttpData;
-import org.jboss.netty.handler.codec.http.multipart.InterfaceHttpData.HttpDataType;
 import org.jboss.netty.logging.InternalLogger;
 import org.jboss.netty.logging.InternalLoggerFactory;
 import org.jboss.netty.util.CharsetUtil;
@@ -60,10 +56,8 @@ public class HttpCSHandler extends SimpleChannelHandler {
 
     private boolean readingChunks;
 
-    private static final HttpDataFactory factory = new DefaultHttpDataFactory(DefaultHttpDataFactory.MINSIZE); // Disk if
-                                                                                                               // MINSIZE
-
     private HttpPostRequestDecoder decoder;
+
     static {
         DiskFileUpload.deleteOnExitTemporaryFile = true; // should delete file
                                                          // on exit (in normal
@@ -83,23 +77,25 @@ public class HttpCSHandler extends SimpleChannelHandler {
         }
     }
 
+    /** Buffer that stores the response content */
+    private final StringBuilder buf = new StringBuilder();
+
     @Override
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
         if (!readingChunks) {
-            // clean previous FileUpload if Any
-            if (decoder != null) {
-                decoder.cleanFiles();
-                decoder = null;
-            }
             HttpRequest request = this.request = (HttpRequest) e.getMessage();
-            logger.info("## " + request.getUri());
+
+            if (is100ContinueExpected(request)) {
+                send100Continue(e);
+            }
+            logger.info("## " + request.getUri() + " ## " + request.getMethod());
             URI uri = new URI(request.getUri());
 
-            if (uri.getPath().startsWith("/")) {
+            if (HttpMethod.POST.equals(request.getMethod())) {
                 QueryStringDecoder queryStringDecoder = new QueryStringDecoder(request.getUri());
                 Map<String, List<String>> params = queryStringDecoder.getParameters();
                 if (params.size() > 0) {
-                    CharSequence result = serviceFactory.create("/").execute(params);
+                    CharSequence result = serviceFactory.create(uri.getPath()).execute(params);
                     logger.info("## " + request.getUri() + " >> " + result);
                     HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
                     response.setContent(ChannelBuffers.copiedBuffer(result, Charsets.UTF_8));
@@ -107,153 +103,112 @@ public class HttpCSHandler extends SimpleChannelHandler {
                 }
             }
 
-            if (!uri.getPath().startsWith("/enonce")) {
-                return;
-            }
-            
-            // if GET Method: should not try to create a HttpPostRequestDecoder
-            try {
-                decoder = new HttpPostRequestDecoder(factory, request);
-                logger.info("## DECODER " + decoder);
-//                for (InterfaceHttpData data : decoder.getBodyHttpDatas()) {
-//                    if (data.getHttpDataType() == HttpDataType.Attribute) {
-//                        Attribute attribute = (Attribute) data;
-//                        String value = attribute.getValue();
-//                        System.out.println("data " + data.getName() + "\n" + value);
-//                     }                     
+            if (HttpMethod.POST.equals(request.getMethod())) {
+                buf.setLength(0);
+//                buf.append("WELCOME TO THE WILD WILD WEB SERVER\r\n");
+//                buf.append("===================================\r\n");
+//
+//                buf.append("VERSION: " + request.getProtocolVersion() + "\r\n");
+//                buf.append("HOSTNAME: " + getHost(request, "unknown") + "\r\n");
+//                buf.append("REQUEST_URI: " + request.getUri() + "\r\n\r\n");
+//
+//                for (Map.Entry<String, String> h : request.getHeaders()) {
+//                    buf.append("HEADER: " + h.getKey() + " = " + h.getValue() + "\r\n");
 //                }
-            } catch (ErrorDataDecoderException e1) {
-                e1.printStackTrace();
-                writeResponse(e.getChannel());
-                Channels.close(e.getChannel());
-                return;
-            } catch (IncompatibleDataDecoderException e1) {
-                writeResponse(e.getChannel());
-                return;
-            }
+//                buf.append("\r\n");
 
-            if (request.isChunked()) {
-                // Chunk version
-                readingChunks = true;
-            } else {
-                // Not chunk version
-                readHttpDataAllReceive(e.getChannel());
-                writeResponse(e.getChannel());
-            }
-        } else {
-            logger.info("## DECODER " + decoder);
-            // New chunk is received
-            HttpChunk chunk = (HttpChunk) e.getMessage();
-            try {
-                decoder.offer(chunk);
-            } catch (ErrorDataDecoderException e1) {
-                e1.printStackTrace();
-                writeResponse(e.getChannel());
-                Channels.close(e.getChannel());
-                return;
-            }
-            readHttpDataChunkByChunk(e.getChannel());
-            if (chunk.isLast()) {
-                readHttpDataAllReceive(e.getChannel());
-                writeResponse(e.getChannel());
-                readingChunks = false;
-            }
-        }
-    }
+//                QueryStringDecoder queryStringDecoder = new QueryStringDecoder(request.getUri());
+//                Map<String, List<String>> params = queryStringDecoder.getParameters();
+//                if (!params.isEmpty()) {
+//                    for (Entry<String, List<String>> p : params.entrySet()) {
+//                        String key = p.getKey();
+//                        List<String> vals = p.getValue();
+//                        for (String val : vals) {
+//                            buf.append("PARAM: " + key + " = " + val + "\r\n");
+//                        }
+//                    }
+//                    buf.append("\r\n");
+//                }
 
-    /**
-     * Example of reading all InterfaceHttpData from finished transfer
-     * 
-     * @param channel
-     */
-    private void readHttpDataAllReceive(Channel channel) {
-        logger.info("readHttpDataAllReceive");
-        List<InterfaceHttpData> datas = null;
-        try {
-            datas = decoder.getBodyHttpDatas();
-            logger.info("readHttpDataAllReceive datas : " + datas);
-        } catch (NotEnoughDataDecoderException e1) {
-            // Should not be!
-            e1.printStackTrace();
-            writeResponse(channel);
-            Channels.close(channel);
-            return;
-        }
-
-        logger.info("readHttpDataAllReceive data size : " + datas.size());
-        for (InterfaceHttpData data : datas) {
-            writeHttpData(data);
-        }
-    }
-
-    private void readHttpDataChunkByChunk(Channel channel) {
-        logger.info("readHttpDataAllReceive");
-        try {
-            while (decoder.hasNext()) {
-                InterfaceHttpData data = decoder.next();
-                if (data != null) {
-                    // new value
-                    writeHttpData(data);
-                }
-            }
-        } catch (EndOfDataDecoderException e1) {
-        }
-    }
-
-    private void writeHttpData(InterfaceHttpData data) {
-        logger.info("writeHttpData");
-        if (data.getHttpDataType() == HttpDataType.FileUpload) {
-            FileUpload fileUpload = (FileUpload) data;
-            if (fileUpload.isCompleted()) {
-                if (fileUpload.length() < 10000) {
-                    try {
-                        logger.info("## Post : \n" + fileUpload.getString(fileUpload.getCharset()));
-                    } catch (IOException e1) {
-                        // do nothing for the example
-                        e1.printStackTrace();
-                    }
+                if (request.isChunked()) {
+                    readingChunks = true;
                 } else {
-                    logger.warn("File too long :" + fileUpload.length());
+                    ChannelBuffer content = request.getContent();
+                    if (content.readable()) {
+                        buf.append(content.toString(CharsetUtil.UTF_8) + "\r\n");
+                    }
+                    writeResponse(e);
                 }
             }
         } else {
-            if (data.getHttpDataType() == HttpDataType.Attribute) {
-                try {
-                Attribute attribute = (Attribute) data;
-                String value = attribute.getValue();
-                System.out.println("data :" + attribute.getName() +"\n" + value);
-                }catch(IOException ex){
-                    logger.warn("read attribute",ex);
-                }
+            HttpChunk chunk = (HttpChunk) e.getMessage();
+            if (chunk.isLast()) {
+                readingChunks = false;
+                writeResponse(e);
+            } else {
+                buf.append(chunk.getContent().toString(CharsetUtil.UTF_8) + "\r\n");
             }
-        }  
+        }
     }
 
-    private void writeResponse(Channel channel) {
+    private void writeResponse(MessageEvent e) {
 
+        System.out.println("---------------\n" + buf.toString() + "\n---------------\n");
         // Decide whether to close the connection or not.
-        boolean close = HttpHeaders.Values.CLOSE.equalsIgnoreCase(request.getHeader(HttpHeaders.Names.CONNECTION))
-                || request.getProtocolVersion().equals(HttpVersion.HTTP_1_0)
-                && !HttpHeaders.Values.KEEP_ALIVE.equalsIgnoreCase(request.getHeader(HttpHeaders.Names.CONNECTION));
+        boolean keepAlive = isKeepAlive(request);
 
         // Build the response object.
-        HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CREATED);
-        response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+        // response.setContent(ChannelBuffers.copiedBuffer(buf.toString(), CharsetUtil.UTF_8));
+        response.setHeader(CONTENT_TYPE, "text/plain; charset=UTF-8");
 
-        if (!close) {
-          response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, "0");
+        if (keepAlive) {
+            // Add 'Content-Length' header only for a keep-alive connection.
+            response.setHeader(CONTENT_LENGTH, response.getContent().readableBytes());
+            // Add keep alive header as per:
+            // - http://www.w3.org/Protocols/HTTP/1.1/draft-ietf-http-v11-spec-01.html#Connection
+            response.setHeader(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
         }
 
+        // Encode the cookie.
+        // String cookieString = request.getHeader(COOKIE);
+        // if (cookieString != null) {
+        // CookieDecoder cookieDecoder = new CookieDecoder();
+        // Set<Cookie> cookies = cookieDecoder.decode(cookieString);
+        // if (!cookies.isEmpty()) {
+        // // Reset the cookies if necessary.
+        // CookieEncoder cookieEncoder = new CookieEncoder(true);
+        // for (Cookie cookie : cookies) {
+        // cookieEncoder.addCookie(cookie);
+        // response.addHeader(SET_COOKIE, cookieEncoder.encode());
+        // }
+        // }
+        // } else {
+        // // Browser sent no cookie. Add some.
+        // // CookieEncoder cookieEncoder = new CookieEncoder(true);
+        // // cookieEncoder.addCookie("key1", "value1");
+        // // response.addHeader(SET_COOKIE, cookieEncoder.encode());
+        // // cookieEncoder.addCookie("key2", "value2");
+        // // response.addHeader(SET_COOKIE, cookieEncoder.encode());
+        // }
+
         // Write the response.
-        ChannelFuture future = channel.write(response);
-        if (close) {
+        ChannelFuture future = e.getChannel().write(response);
+
+        // Close the non-keep-alive connection after the write operation is done.
+        if (!keepAlive) {
             future.addListener(ChannelFutureListener.CLOSE);
         }
     }
 
+    private static void send100Continue(MessageEvent e) {
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, CONTINUE);
+        e.getChannel().write(response);
+    }
+
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
-        logger.error("exceptionCaught", e.getCause());
+        e.getCause().printStackTrace();
         e.getChannel().close();
     }
 }
